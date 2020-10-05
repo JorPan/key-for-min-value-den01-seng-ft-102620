0c7aa81af8baa062a8576ee5966ca40c9b81d03b
Jordan Panasewicz <jbpanasewicz@gmail.com>
2020-10-05 19:06:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,16 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if name_hash.length == 0 
-      return nil
-    end
-  key = ""
-  currentmin = 999,999,999,999 
-  name_hash.each do |k, v|
-    if v < currentmin 
-      currentmin = v
-      key = k 
-    end
-  end 
-  return key 
+  winner = ""
+  min = 
+  name_hash.each do |key, value|
+    if value 
+      
+    
 end
