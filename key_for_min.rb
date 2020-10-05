168af69a923bf314027214ff106b4c772b345707
Jordan Panasewicz <jbpanasewicz@gmail.com>
2020-10-05 19:11:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,11 +7,9 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if name_hash.count == 0 
       return nil 
-    if value <= min_val
+    elsif value <= min_val
       min_val = value
       min_key = key 
-    else 
-      return nil
     end
   end
   return min_key
